a895324eef29945e674081fe631dca194dce17b4
Arthurferrera <arthur_f.oliveira@hotmail.com>
2018-07-14 17:23:13 -0300

    Api fotos

---
+++ src/app/foto/foto.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'foto',
+    template: `<img [src]="url" [alt]="titulo">`,
+    styles: []
+})
+export class FotoComponent {
+    @Input() url = '';
+    @Input() titulo = '';
+
+
+    
+}
